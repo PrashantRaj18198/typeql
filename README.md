0be12965008dbcba2f847851c7c76e022a65283c
PrashantRaj18198 <prashantraj18198@gmail.com>
2021-03-27 00:24:22 +0530

    add comments in function

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # typeql
+
 Better way to provide typings for your graphql queries.
+
+# How to use
+
+- Put files inside tql folder with .tql extension
+- `npm run start`
+- Get .graphql files in root folder
--- src/converter.ts
+++ src/converter.ts
@@ -1,11 +1,28 @@
 const fs = require("fs").promises;
+/**
+ * @todo handle variables in query. This can be done by ignoring everyting
+ * inside parantheses `()`.
+ * @todo provide typings for the query result in a .ts file
+ * @todo provide typings for the variables in a .ts file
+ */
 export async function tql(): Promise<void> {
   try {
     const data = await fs.readFile("tql/query-basic.tql");
-    // console.log(Buffer.from(data).toString());
+    // convert file data to utf-8 and a string
     let query = Buffer.from(data, "utf-8").toString();
+    /**
+     * replace everything after a : till the end of line `$` or till you find a
+     * `{` character. This will also remove `[` since `[` always comes before a
+     * `{`. Check query-basic.tql (line 4)
+     */
     query = query.replace(/:.*(?<!{)/gm, " ");
+    /**
+     * remove the `]` character from the query
+     */
     query = query.replace("]", "");
+    /**
+     * write the query to a .graphql file with the same name
+     */
     await fs.writeFile("query-basic.graphql", query);
   } catch (error) {
     console.log(error);
