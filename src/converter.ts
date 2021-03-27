f2290cf86219e2687e316f125670b6bee86ff028
PrashantRaj18198 <prashantraj18198@gmail.com>
2021-03-27 13:41:18 +0530

    add documentation, create graphql files for all tql files

--- src/converter.ts
+++ src/converter.ts
@@ -1,45 +1,45 @@
 const graphqlFolder = "./graphql/";
-const fileName = `query-with-variables`;
+// const fileName = `query-with-variables`;
 const fs = require("fs").promises;
 /**
- * @todo handle variables in query. This can be done by ignoring everyting
- * inside parantheses `()`.
  * @todo provide typings for the query result in a .ts file
  * @todo provide typings for the variables in a .ts file
  */
-export async function tql(): Promise<void> {
+export async function tql(fileNameWithEnding: string): Promise<void> {
+  // remove the .tql from the fileName passed
+  const fileName = fileNameWithEnding.split(".")[0];
   try {
-    await createGraphqlFolder();
     console.log("running tql converter");
+    // read data from tql file
     const data = await fs.readFile(`tql/${fileName}.tql`);
     // convert file data to utf-8 and a string
     let query = Buffer.from(data, "utf-8").toString();
+    /**
+     * we don't need to make changes to anything inside the `(.....)` so match
+     * everything till the last `)` as position say `x` and slice till this
+     * posiion `x`, and keep in slicedQuery as we don't need to remove typings
+     * from this part.
+     */
     let slicedQuery = "";
-    let m;
+    /**
+     * parantheses matcher. Matches everything till the last `)` character found
+     * Since we only support one query per file this should be okay to do.
+     * @todo allow multiple queries.
+     */
     const matchParanthesesRegex = /\((.|\n)*\)/gm;
     const match = matchParanthesesRegex.exec(query);
     console.log("matched @", match && match?.index + match[0].length);
+    /**
+     * if match is found, i.e., variables are part of this query we can slice it
+     * and keep it seperate. otherwise its just an empty string
+     */
     if (match) {
       slicedQuery = query.slice(0, match.index + match[0].length);
       console.log("slicedQuery", slicedQuery, "\n\n");
       query = query.slice(match.index + match[0].length);
       console.log("query", query, "\n\n");
     }
-    // console.log("matches", matches);
-    // matches?.forEach((match, groupIndex) => {
-    //   console.log(`Found match, group ${groupIndex}: ${match}: ${match}`);
-    // });
-    // while ((m = matchParanthesesRegex.exec(query)) !== null) {
-    //   // This is necessary to avoid infinite loops with zero-width matches
-    //   if (m.index === matchParanthesesRegex.lastIndex) {
-    //     matchParanthesesRegex.lastIndex++;
-    //   }
 
-    //   // The result can be accessed through the `m`-variable.
-    //   m.forEach((match, groupIndex) => {
-    //     console.log(`Found match, group ${groupIndex}: ${match}`);
-    //   });
-    // }
     /**
      * replace everything after a : till the end of line `$` or till you find a
      * `{` character. This will also remove `[` since `[` always comes before a
@@ -50,6 +50,9 @@ export async function tql(): Promise<void> {
      * remove the `]` character from the query
      */
     query = query.replace("]", "");
+    /**
+     * merge the queries.
+     */
     query = slicedQuery + query;
     console.log("output", query);
 
@@ -59,12 +62,6 @@ export async function tql(): Promise<void> {
     await fs.writeFile(`${graphqlFolder}/${fileName}.graphql`, query);
   } catch (error) {
     console.log(error);
-  }
-}
-
-async function createGraphqlFolder(): Promise<void> {
-  if (!!!(await fs.stat(graphqlFolder).catch((e: Error) => false))) {
-    await fs.mkdir(graphqlFolder);
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { tql } from "./converter";
+import { promises as fs } from "fs";
 
-// read data from tql folder
-// console.log(data);
-// const HeroNameAndFriends = `query HeroNameAndFriends {
-//   hero: {
-//     name: String!
-//     friends: [{
-//       name: String!
-//     }]
-//   }
-// }`;
+const graphqlFolder = "./graphql/";
 
-// const HeroName = `query HeroName {
-//   name: String!
-// }`;
-tql();
+async function createGraphqlFolder(): Promise<void> {
+  if (!!!(await fs.stat(graphqlFolder).catch((e: Error) => false))) {
+    await fs.mkdir(graphqlFolder);
+  }
+}
+
+async function createGraphQLFilesForEachTypeQlFile(): Promise<void> {
+  // create graphql folder if not exists
+  await createGraphqlFolder();
+  // read all files in tql folder
+  const folder = await fs.readdir("./tql");
+  // for each tql file run the converter
+  folder.forEach((file) => tql(file));
+}
+
+createGraphQLFilesForEachTypeQlFile();
